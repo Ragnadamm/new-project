4137a62504fe04590f6f169cbbc32f3c99a7d195
Felipe Pontes <felipe92pontes@gmail.com>
2020-10-27 02:11:17 -0300

    Add all class

--- src/main/java/com/project/professor/allocation/model/Course.java
+++ src/main/java/com/project/professor/allocation/model/Course.java
@@ -1,5 +1,42 @@
 package com.project.professor.allocation.model;
 
-public class Course {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "course")
+public class Course {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", nullable = false, unique = true)
+	private String name;
+	
+	
+	public Course() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
